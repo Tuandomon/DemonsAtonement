e8295afe0bf30b7be4e4cd64dc3231fc5d46dd86
Xuanharu <xuan12345.912.pch@gmail.com>
2025-10-18 19:05:10 +0700

    Health player

---
+++ Assets/Xuan/PlayerHealth.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [Header("Health Settings")]
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Gọi hàm này khi player nhận sát thương
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        Debug.Log("Player took damage: " + damage + " | Current HP: " + currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    // Gọi hàm này để hồi máu
+    public void Heal(int amount)
+    {
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        Debug.Log("Player healed: " + amount + " | Current HP: " + currentHealth);
+    }
+
+    // Hàm xử lý khi player chết
+    void Die()
+    {
+        Debug.Log("Player has died!");
+        // Thêm logic chết như animation, reload scene, v.v.
+    }
+}
