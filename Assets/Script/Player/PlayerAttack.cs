f4d971d4d9e38520a14c88fab67fc2ee5d6a4828
Minh Tuấn <minhtuan5104@gmail.com>
2025-10-18 19:45:15 +0700

    Player Attack

---
+++ Assets/Script/Player/PlayerAttack.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public float attackCooldown = 1f; // Thời gian hồi chiêu giữa các lần đánh
+    private float lastAttackTime = -Mathf.Infinity;
+
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        // Kiểm tra nhấn chuột trái và đủ thời gian hồi chiêu
+        if (Input.GetMouseButtonDown(0) && Time.time >= lastAttackTime + attackCooldown)
+        {
+            FlipToMouse();     // Xoay hướng theo chuột
+            Attack();          // Gọi animation và logic tấn công
+        }
+    }
+
+    void FlipToMouse()
+    {
+        // Lấy vị trí chuột trong thế giới
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // So sánh vị trí chuột với vị trí nhân vật
+        float direction = mouseWorldPos.x - transform.position.x;
+
+        // Lật sprite nếu chuột bên trái
+        spriteRenderer.flipX = direction < 0;
+    }
+
+    void Attack()
+    {
+        // Kích hoạt animation tấn công
+        animator.SetTrigger("attack");
+
+        // Ghi lại thời gian tấn công
+        lastAttackTime = Time.time;
+
+        // Viết thêm logic gây sát thương nếu cần
+        Debug.Log("Player tấn công!");
+    }
+}
