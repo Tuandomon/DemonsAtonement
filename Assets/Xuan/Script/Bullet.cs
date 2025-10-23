eb9b827ca6278dce6b72bbf93e2f061d995261c4
Xuanharu <xuan12345.912.pch@gmail.com>
2025-10-23 22:05:46 +0700

    skill bắn đạn

---
+++ Assets/Xuan/Script/Bullet.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float maxDistance = 10f;
+    public float speed = 20f;
+
+    private Vector2 startPosition;
+    private float direction = 1f; // mặc định bên phải
+
+    public void SetDirection(float dir)
+    {
+        direction = dir;
+    }
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        // Di chuyển theo hướng mặt
+        transform.Translate(Vector2.right * direction * speed * Time.deltaTime);
+
+        // Kiểm tra khoảng cách
+        float distanceTraveled = Vector2.Distance(transform.position, startPosition);
+        if (distanceTraveled >= maxDistance)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
