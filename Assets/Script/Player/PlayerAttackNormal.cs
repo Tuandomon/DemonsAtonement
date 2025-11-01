d59b6145008593298a76fa70534604c9cf7b0660
Minh Tuấn <minhtuan5104@gmail.com>
2025-11-01 18:05:06 +0700

    đánh thường vs hit dmg

---
+++ Assets/Script/Player/PlayerAttackNormal.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlayerAttackNormal : MonoBehaviour
+{
+    private Animator animator;
+    private int attackStep = 1;
+    private float lastAttackTime = 0f;
+    public float comboWindow = 0.5f;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float currentTime = Time.time;
+
+        if (currentTime - lastAttackTime > comboWindow)
+        {
+            attackStep = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            animator.SetInteger("AttackNormalStep", attackStep);
+            animator.SetTrigger("AttackNormalTrigger");
+
+            lastAttackTime = currentTime;
+            attackStep++;
+            if (attackStep > 2) attackStep = 1;
+        }
+    }
+}
