b2f91be271030fcd73b7a5e46f9194ba18e5864c
anhchien2509 <chienlaps38508@gmail.com>
2025-11-01 15:32:56 +0700

    lam UI nut bam cho cua va code dich chuyen cho cua
    ...

---
+++ Assets/Chien/Scripts/ShowButtonWhenNear.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class ShowButtonWhenNear : MonoBehaviour
+{
+    public GameObject buttonUI; // gán Button_cua vào đây trong Inspector
+
+    private void Start()
+    {
+        if (buttonUI != null)
+            buttonUI.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) // player phải có tag Player
+        {
+            if (buttonUI != null)
+                buttonUI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (buttonUI != null)
+                buttonUI.SetActive(false);
+        }
+    }
+}
