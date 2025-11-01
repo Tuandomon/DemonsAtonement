80547838f693666e62e6d643786a70c65425cf35
Minh Tuấn <minhtuan5104@gmail.com>
2025-11-01 18:31:36 +0700

    Attack Normal

---
+++ Assets/Script/Player/HitboxDamage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitboxDamage : MonoBehaviour
+{
+    public int damage = 10;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            // Gọi hàm nhận sát thương bên enemy
+            other.GetComponent<EnemyHealth>()?.TakeDamage(damage);
+        }
+    }
+}
--- Assets/Script/Player/PlayerAttackNormal.cs
+++ Assets/Script/Player/PlayerAttackNormal.cs
@@ -3,9 +3,34 @@
 public class PlayerAttackNormal : MonoBehaviour
 {
     private Animator animator;
-    private int attackStep = 1;
     private float lastAttackTime = 0f;
+    private bool waitingForSecondAttack = false;
     public float comboWindow = 0.5f;
+    public class PlayerAttackHitbox : MonoBehaviour
+    {
+        public GameObject hitbox1;
+        public GameObject hitbox2;
+
+        public void EnableHitbox1()
+        {
+            hitbox1.SetActive(true);
+        }
+
+        public void DisableHitbox1()
+        {
+            hitbox1.SetActive(false);
+        }
+
+        public void EnableHitbox2()
+        {
+            hitbox2.SetActive(true);
+        }
+
+        public void DisableHitbox2()
+        {
+            hitbox2.SetActive(false);
+        }
+    }
 
     void Start()
     {
@@ -16,19 +41,29 @@ public class PlayerAttackNormal : MonoBehaviour
     {
         float currentTime = Time.time;
 
-        if (currentTime - lastAttackTime > comboWindow)
+        // Nếu đang chờ combo nhưng hết thời gian → reset
+        if (waitingForSecondAttack && currentTime - lastAttackTime > comboWindow)
         {
-            attackStep = 1;
+            waitingForSecondAttack = false;
         }
 
-        if (Input.GetKeyDown(KeyCode.J))
+        if (Input.GetKeyDown(KeyCode.J) && !Input.GetKey(KeyCode.S))
         {
-            animator.SetInteger("AttackNormalStep", attackStep);
-            animator.SetTrigger("AttackNormalTrigger");
+            AnimatorStateInfo state = animator.GetCurrentAnimatorStateInfo(0);
 
-            lastAttackTime = currentTime;
-            attackStep++;
-            if (attackStep > 2) attackStep = 1;
+            if (state.IsName("Idle"))
+            {
+                // Đánh đòn 1
+                animator.SetTrigger("AttackNormalTrigger");
+                waitingForSecondAttack = true;
+                lastAttackTime = currentTime;
+            }
+            else if (state.IsName("WaitForInput") && waitingForSecondAttack)
+            {
+                // Đánh đòn 2
+                animator.SetTrigger("AttackNormalTrigger");
+                waitingForSecondAttack = false;
+            }
         }
     }
 }
