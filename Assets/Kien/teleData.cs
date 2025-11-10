4bdd1cc03914e998ca01fbb1bac1d995a5d4bb6e
kien0107 <nguyentrungkien172k4@gmail.com>
2025-11-10 19:48:58 +0700

    chua xong

---
+++ Assets/Kien/teleData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleData : MonoBehaviour
+{
+   
+        public static string spawnPointName = "Door2";   // tên object spawn trong scene đích
+        public static bool useSpawnPoint = false;
+ 
+}
