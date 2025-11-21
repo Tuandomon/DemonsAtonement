315f89d3dac02462d33ad94ef7f0a05e9c4be297
Minh Tuấn <minhtuan5104@gmail.com>
2025-11-21 19:16:08 +0700

    Làm xu và key, hiển thị UI

---
+++ Assets/Minh Tuan/Script/Coin and key/KeyItem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class KeyItem : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            // Gọi xử lý nhặt key từ Player nếu cần
+            Debug.Log("Người chơi đã nhặt chìa khóa!");
+
+            // Xóa chìa khóa khỏi scene
+            Destroy(gameObject);
+        }
+    }
+}
