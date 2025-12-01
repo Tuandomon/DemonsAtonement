5e8854b0f408ea4ccb57344e70abc8591bbf3b6a
anhchien2509 <chienlaps38508@gmail.com>
2025-12-01 14:08:35 +0700

    làm video huong dan
    ...

---
+++ Assets/Chien/Scripts/PanelToggleWithVideo.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class PanelToggleWithVideo : MonoBehaviour
+{
+    [Header("UI & Video References")]
+    public GameObject Panel_ins;          // Panel muốn bật/tắt
+    public Button Button_instruct;        // Nút click để toggle
+    public VideoPlayer videoPlayer;       // VideoPlayer gắn trên panel
+
+    private bool isPanelActive = false;
+
+    void Start()
+    {
+        if (Button_instruct != null)
+        {
+            Button_instruct.onClick.AddListener(TogglePanel);
+        }
+
+        if (Panel_ins != null)
+        {
+            Panel_ins.SetActive(isPanelActive);
+        }
+    }
+
+    void Update()
+    {
+        // Nhấn phím L cũng toggle
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            TogglePanel();
+        }
+    }
+
+    public void TogglePanel()
+    {
+        isPanelActive = !isPanelActive;
+
+        if (Panel_ins != null)
+        {
+            Panel_ins.SetActive(isPanelActive);
+        }
+
+        if (videoPlayer != null)
+        {
+            if (isPanelActive)
+            {
+                // Khi bật panel, phát lại video từ đầu
+                videoPlayer.time = 0;
+                videoPlayer.Play();
+            }
+            else
+            {
+                // Khi tắt panel, dừng video và reset về đầu
+                videoPlayer.Stop();
+                videoPlayer.time = 0;
+            }
+        }
+    }
+}
