98bcdbe30a62ac649ec978bda8dfb31b13b4d778
anhchien2509 <chienlaps38508@gmail.com>
2025-12-04 14:26:14 +0700

    Code cho Tui do va item chu
    ...

---
+++ Assets/Chien/Scripts/LetterPickup.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class LetterPickup : MonoBehaviour
+{
+    public Sprite icon;        // Hình mảnh chữ
+    public string itemName;    // Tên mảnh chữ
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            InventoryItem.instance.AddItem(icon, itemName);
+            Destroy(gameObject);
+        }
+    }
+}
