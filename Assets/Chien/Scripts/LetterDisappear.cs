fe6968ca63fae97822c204cff10f703b85f3148b
anhchien2509 <chienlaps38508@gmail.com>
2025-12-04 13:29:44 +0700

    xoa asset chu di
    ...

---
+++ Assets/Chien/Scripts/LetterDisappear.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class LetterDisappear : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);   // xoá chữ
+        }
+    }
+}
