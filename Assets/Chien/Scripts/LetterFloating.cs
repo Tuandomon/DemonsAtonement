424b0c186f192af8d9e46eadef127a0164c7feac
anhchien2509 <chienlaps38508@gmail.com>
2025-12-04 13:53:28 +0700

    them code va asset cho item chu
    ...

---
+++ Assets/Chien/Scripts/LetterFloating.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class LetterFloating : MonoBehaviour
+{
+    public float amplitude = 0.2f;   // độ cao dao động
+    public float speed = 2f;         // tốc độ dao động
+
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        float newY = startPos.y + Mathf.Sin(Time.time * speed) * amplitude;
+        transform.position = new Vector3(startPos.x, newY, startPos.z);
+    }
+}
