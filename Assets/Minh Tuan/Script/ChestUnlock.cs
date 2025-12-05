1e4c69ceedecd4a42ff0f507ad0860b3e7f17816
Minh Tuấn <minhtuan5104@gmail.com>
2025-12-05 20:28:38 +0700

    rương mửo pannel khóa

---
+++ Assets/Minh Tuan/Script/ChestUnlock.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class ChestUnlock : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        if (animator != null)
+        {
+            animator.Play("ChestOpen"); // tên clip mở
+        }
+    }
+
+    void Update()
+    {
+        if (animator != null)
+        {
+            AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+            if (stateInfo.IsName("ChestOpen") && stateInfo.normalizedTime >= 1f)
+            {
+                // animation đã chạy hết → dừng lại
+                animator.enabled = false;
+            }
+        }
+    }
+}
